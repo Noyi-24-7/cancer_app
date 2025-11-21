ee887a7ac628f2396bb41f4ada2af6cbcb041646
Adinoyi Ismaila <noyi@Adinoyis-MacBook-Pro-2.local>
2025-11-21 10:08:04 +0100

    fix: update package name from preggos to cancercare in test and web config
    - Fixed test/widget_test.dart package import from preggos to cancercare
    - Updated web/index.html meta tags and title from Preggos to CancerCare
    - Resolves pub dependency error: name field doesn't match expected name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:preggos/main.dart';
+import 'package:cancercare/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
